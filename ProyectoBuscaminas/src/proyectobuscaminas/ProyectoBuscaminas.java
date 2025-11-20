8b117413eefe22bd7c4ac0a843d19ad48337ba22
Darwin <93103712+darwin712@users.noreply.github.com>
2025-11-19 20:27:28 -0800

    inicios.
    se creo el menu

--- ProyectoBuscaminas/src/proyectobuscaminas/ProyectoBuscaminas.java
+++ ProyectoBuscaminas/src/proyectobuscaminas/ProyectoBuscaminas.java
@@ -16,7 +16,7 @@ public class ProyectoBuscaminas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Testeo frame = new Testeo();
+        Menu frame = new Menu();
         
         SwingUtilities.invokeLater(() -> {
             frame.setVisible(true);
